64873b31da6d2163570e82d717e8e6b8f827e446
Shamim Ahamed <Shamim.aiub2@gmail.com>
2020-10-21 02:48:16 +0600

    last commit

--- Script.js
+++ Script.js
@@ -4,6 +4,31 @@ $(document).ready(function ()
         $().toggleClass('open');
         $(".top-nav").toggleClass('open');
     });
- 
-   
+
+    $('.top-nav .nav-link').on('click', function(){
+        $('.menu-toggler').removeClass('open');
+        $(".top-nav").removeClass('open');
+    });
+    $('.nav a [href*="#"]').on('click', function(){
+        $('html, body').animate (keyframes={
+            scrollTop: $($(this).attr('href')).offset().top -100
+        }, options=2000);
+        $(".top-nav").toggleClass('open');
+    });
+
+   $('#up').on('click',function () {
+    $('html, body').animate( keyframes= {
+        scrollTop: 0
+      }, options=2000);
+   });
+
+   $('#up').on('click', function(){
+    $('html, body').animate (keyframes={
+        scrollTop: 0
+    },options=2000);
+    });
+    AOS.init({
+        duration: 1200,
+      })
+  
 });
